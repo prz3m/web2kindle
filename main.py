bba2a3519f85e18bc95f1feb6b049c2da412de08
prz3m <ppmmog@gmail.com>
2016-09-25 22:09:49 +0200

    editing settings inside app

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class Form(QMainWindow, Ui_Dialog):
         c.convert()
 
     def open_settings(self):
-        
+
         settings.show()
 
 
@@ -51,6 +51,29 @@ class Settings(QMainWindow, Ui_Settings):
 
         # Set up the user interface from Designer.
         self.setupUi(self)
+
+        self.load_settings()
+        self.pushButton.clicked.connect(self.save_settings)
+
+    def load_settings(self):
+        config = configparser.ConfigParser()
+        config.read("web2kindle.conf")
+        if "web2kindle" in config.keys() and "gmail_login" in config["web2kindle"]:
+            login = config["web2kindle"]["gmail_login"]
+            self.lineEdit.setText(login)
+        if "web2kindle" in config.keys() and "kindle_address" in config["web2kindle"]:
+            kindle = config["web2kindle"]["kindle_address"]
+            self.lineEdit_2.setText(kindle)
+
+    def save_settings(self):
+        config = configparser.ConfigParser()
+        config["web2kindle"] = {}
+        config["web2kindle"]["gmail_login"] = self.lineEdit.text()
+        config["web2kindle"]["kindle_address"] = self.lineEdit_2.text()
+        with open("web2kindle.conf", "w") as f:
+            config.write(f)
+        settings.hide()
+
 
 app = QApplication(sys.argv)
 form = Form()
